a2be878d18cca08a9b4940b051baea1b213326c0
Imsyces <119516739+Imsyces@users.noreply.github.com>
2023-04-26 20:07:38 +0300

    Update HatchClient.lua

--- HatchClient.lua
+++ HatchClient.lua
@@ -1,66 +1,66 @@
-local replicatedstroage = game:GetService("ReplicatedStorage") -- ReplicatedStorage
-local runService = game:GetService("RunService") -- RunService
-local tweenservice = game:GetService("TweenService") -- TweenService Using for animate
-local uis = game:GetService("UserInputService") -- UserInputService
+local replicatedstroage = game:GetService("ReplicatedStorage")
+local runService = game:GetService("RunService")
+local tweenservice = game:GetService("TweenService")
+local uis = game:GetService("UserInputService")
 
 
-local camera = workspace.Camera -- Camera
-local player = game.Players.LocalPlayer -- Player
+local camera = workspace.Camera
+local player = game.Players.LocalPlayer
 
-local Pillows = replicatedstroage:WaitForChild("Pillows") -- replicatedstroage.Pillows
-local Boxs = workspace:WaitForChild("Boxs") -- workspace.Boxs
-local module3D = require(replicatedstroage:WaitForChild("Module3D")) --replicatedstroage.Module3D
+local Pillows = replicatedstroage:WaitForChild("Pillows")
+local Boxs = workspace:WaitForChild("Boxs")
+local module3D = require(replicatedstroage:WaitForChild("Module3D"))
 
 
-local maxDisplayDistance = 15 -- Display Distance for billboards
-local canHatch = false -- If true then player can hatch box
-local isHatching = false -- Is Hatching Now 
-local hatchOneConnection = nil -- Connection
+local maxDisplayDistance = 15
+local canHatch = false
+local isHatching = false
+local hatchOneConnection = nil
 
-wait(.5) -- Wait half seconds
+wait(.5)
 
-local function animateBillboard(billboard, openOrClose) -- bilboard: Instance, openOrClose: Bool
-
-	if openOrClose == true then -- open 
-		tweenservice:Create(billboard,TweenInfo.new(.1),{Size = UDim2.new(7,0,8,0)}):Play() -- Play Tween (Animation)
+local function animateBillboard(billboard, openOrClose)
+	if openOrClose == true then
+		tweenservice:Create(billboard,TweenInfo.new(.1),{Size = UDim2.new(7,0,8,0)}):Play()
 	else
-		tweenservice:Create(billboard,TweenInfo.new(.1),{Size = UDim2.new(0,0,0,0)}):Play() -- Stop Tween 
-		wait(.1) 
-		billboard.Enabled = false -- Close billboard (Player Can't see)
+		tweenservice:Create(billboard,TweenInfo.new(.1),{Size = UDim2.new(0,0,0,0)}):Play()
+		wait(.1)
+		billboard.Enabled = false
 		
 	end
-	wait(.5)  -- Wait half seconds
+	wait(.5)
 end
 	
-	for i, v in pairs(Boxs:GetChildren()) do -- get boxs childrens
-	local boxPillows = Pillows:FindFirstChild(v.Name) -- find box in pillows folder
+	for i, v in pairs(Boxs:GetChildren()) do
+	local boxPillows = Pillows:FindFirstChild(v.Name)
 	
-	if boxPillows ~= nil then -- If box's found then
-		local billboardTemplate = script.Template:Clone() -- clone template
-		local Container = billboardTemplate:WaitForChild("Container") -- billboardTemplate.Container
-		local mainFrame = Container:WaitForChild("MainFrame") --Container.MainFrame
+	if boxPillows ~= nil then
+		local billboardTemplate = script.Template:Clone()
+		local Container = billboardTemplate:WaitForChild("Container")
+		local mainFrame = Container:WaitForChild("MainFrame")
 		local template = mainFrame:WaitForChild("Template") --button template
-		local display = template:WaitForChild("Display") -- template.Display
+		local display = template:WaitForChild("Display")
 		
 		
-		billboardTemplate.Parent = script.Parent.Parent.BoxBillboards -- billboardTemplate Parent => BoxBillboard
-		billboardTemplate.Name = v.Name -- Box's name
-		billboardTemplate.Adornee = v.Box -- Display on Box
-		billboardTemplate.Enabled = true -- Player Can see
+		billboardTemplate.Parent = script.Parent.Parent.BoxBillboards
+		billboardTemplate.Name = v.Name
+		billboardTemplate.Adornee = v.Box
+		billboardTemplate.Btn.Adornee = v.Box
+		billboardTemplate.Enabled = true
 		
-		local pillows = {} -- Pillows
+		local pillows = {}
 		
-		for x, pillow in pairs(boxPillows:GetChildren()) do -- Get pillows in box
-			table.insert(pillows,pillow.Rarity.Value) -- Inserts Pillow Rarity
+		for x, pillow in pairs(boxPillows:GetChildren()) do
+			table.insert(pillows,pillow.Rarity.Value)
 		end
 		
-		table.sort(pillows) -- Sort Pillows by Rarities
-		for i = 1, math.floor(#pillows/2) do -- Sort Pillows by Rarities
+		table.sort(pillows)
+		for i = 1, math.floor(#pillows/2) do
 			local j = #pillows - i + 1
 			pillows[i], pillows[j] = pillows[j], pillows[i]
 		end
 		
-		for _, rarity in pairs(pillows) do --  Get rarity in Each pillow
+		for _, rarity in pairs(pillows) do
 			
 		
 		for _, pillow in pairs(boxPillows:GetChildren()) do
@@ -79,8 +79,8 @@ end
 					pillowModel.Camera.FieldOfView = 5
 					pillowModel.Visible = true
 
-					runService.RenderStepped:Connect(function() 
-						pillowModel:SetCFrame(CFrame.Angles(0,0,0) * CFrame.Angles(math.rad(-10),0,0)) -- rotation module3D
+					runService.RenderStepped:Connect(function()
+						pillowModel:SetCFrame(CFrame.Angles(0,0,0) * CFrame.Angles(math.rad(-10),0,0))
 					end)
 					break
 				else
@@ -103,7 +103,6 @@ end
 	end
 end
 _G.hatchOne = function(pillowName,box)
-
 	print(pillowName)
 	local pillow = Pillows[box.Name]:FindFirstChild(pillowName):Clone()
 
@@ -111,7 +110,7 @@ _G.hatchOne = function(pillowName,box)
 	isHatching = true
 	local boxModel = box:FindFirstChild("Box"):Clone()
 	for i,v in pairs(boxModel:GetChildren()) do
-		if v:IsA("BasePart") then
+		if v:IsA("BasePart") and not v:IsA("BillboardGui") then
 			v.Anchored = true
 			v.CanCollide = false
 		end
@@ -124,7 +123,7 @@ _G.hatchOne = function(pillowName,box)
 	boxModel.Parent = camera
 	wait(3)
 	for i,v in pairs(boxModel:GetChildren()) do
-		if v:IsA("BasePart") then
+		if v:IsA("BasePart") and not v:IsA("BillboardGui") then
 			local tape = v:FindFirstChild("Tape")
 			
 			tweenservice:Create(v,TweenInfo.new(0.5),{Transparency = 1}):Play()
@@ -137,6 +136,7 @@ _G.hatchOne = function(pillowName,box)
 		boxModel:Destroy()
 		
 		script.Parent.pillowDisplay.Visible = true
+		script.Parent.pillowDisplay.Pillow.Text = pillowName
 		local pillowModel = module3D:Attach3D(script.Parent.pillowDisplay, pillow)
 		pillowModel:SetDepthMultiplier(1.2)
 		pillowModel.Camera.FieldOfView = 5
@@ -154,9 +154,10 @@ _G.hatchOne = function(pillowName,box)
 			end
 		end
 		isHatching = false
-    
 		script.Parent.pillowDisplay.Visible = false
 	end
+
+
 end
 
 
@@ -187,15 +188,53 @@ uis.InputBegan:Connect(function(input, GPE)
 
 			end
 			
-			if canHatch == true  then 
+			if canHatch == true  then
 				
-				local result = replicatedstroage:WaitForChild("Remotes"):WaitForChild("HatchServer"):InvokeServer(nearestBox) -- Random pillow
-				if result ~= nil then -- If its not empty then
+				local result = replicatedstroage:WaitForChild("Remotes"):WaitForChild("HatchServer"):InvokeServer(nearestBox)
+				if result ~= nil then
 					
-					_G.hatchOne(result,nearestBox) -- Hatch box
+					_G.hatchOne(result,nearestBox)
 			end
 			end
 			
 		end
 	end
 end)
+
+for i,v in pairs(script.Parent.Parent.BoxBillboards:GetChildren()) do
+	local Ebtn = v.Btn.TextButton
+	
+	Ebtn.MouseButton1Click:Connect(function()
+		if  player.Character ~= nil and isHatching == false  then
+			local nearestBox
+			local plrPos = player.Character.HumanoidRootPart.Position
+
+			for i,v in pairs(Boxs:GetChildren()) do
+				if nearestBox == nil then
+					nearestBox = v
+				else
+					if (plrPos - v.PrimaryPart.Position).Magnitude < (nearestBox.PrimaryPart.Position - plrPos).Magnitude then
+						nearestBox = v
+
+					end
+				end
+			end
+			if player:DistanceFromCharacter(nearestBox.Box.PrimaryPart.Position) < maxDisplayDistance then
+
+				canHatch = true
+			else
+				canHatch = false
+
+			end
+			if canHatch == true  then
+
+				local result = replicatedstroage:WaitForChild("Remotes"):WaitForChild("HatchServer"):InvokeServer(nearestBox)
+				if result ~= nil then
+
+					_G.hatchOne(result,nearestBox)
+				end
+			end
+
+		end
+		end)
+end
